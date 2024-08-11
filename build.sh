61b4c94c129f1f03e0803d4d8ba9c33c0976872c
Aaron Speedy <aaron@lspeedy.org>
2024-08-11 17:44:41 -0500

    Fix everything

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# set -xe
+
 gen() {
   echo "Building $1"
-  line=$(head -n 1 docs/$1.md)
-  sed -e "s~PAGE_TITLE~$line~" -e "s~#~~" recs/header.html > docs/$1.html
-  smu docs/$1.md >> docs/$1.html
+  # line=$(head -n 1 docs/$1.md)
+  # sed -e "s~PAGE_TITLE~$line~" -e "s~#~~" recs/$2.html > docs/$1.html
+  cat recs/$2.html > docs/$1.html
+  smu src/$1.md >> docs/$1.html
   cat recs/footer.html >> docs/$1.html
 }
 
@@ -13,4 +16,6 @@ mkdir docs
 cp recs/style.css docs
 
 # mkdir docs/articles
-# gen "articles/tiny-site-generator"
+# gen "articles/tiny-site-generator" header
+
+gen "index" index-header
